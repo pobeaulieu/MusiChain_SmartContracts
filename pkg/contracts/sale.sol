874849365dc66280288f454f2d04e7d30fe98d1b
Eliott Baltz <eliott.baltz.1@ens.etsmtl.ca>
2023-07-31 14:13:34 -0400

    Correction listing

--- pkg/contracts/sale.sol
+++ pkg/contracts/sale.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract Sale {
     struct Listing {
+        uint256 listingId;
         uint256 tokenId;
         address seller;
         uint256 price;
@@ -13,8 +14,11 @@ contract Sale {
     }
 
     IERC1155 public tokenContract;
-    uint256[] public tokenIds;
+    uint256 public listingCount;
     mapping(uint256 => Listing) public listings;
+    mapping(uint256 => uint256[]) public tokenToListings;
+    mapping (address => uint256[]) public ownedTokens;
+    mapping (address => mapping(uint256 => bool)) public hasToken;
 
     constructor(IERC1155 _tokenContract) {
         tokenContract = _tokenContract;
@@ -25,7 +29,13 @@ contract Sale {
         require(priceInWei > 0, "Price should be greater than zero");
         require(amount > 0, "Amount should be greater than zero");
 
-        listings[tokenId] = Listing({
+        if (!hasToken[msg.sender][tokenId]) {
+            ownedTokens[msg.sender].push(tokenId);
+            hasToken[msg.sender][tokenId] = true;
+        }
+
+        listings[listingCount] = Listing({
+            listingId: listingCount,
             tokenId: tokenId,
             seller: msg.sender,
             price: priceInWei,
@@ -33,12 +43,13 @@ contract Sale {
             isForSale: true
         });
 
-        tokenIds.push(tokenId);
+        tokenToListings[tokenId].push(listingCount);
+        listingCount++;
     }
 
-    function buyToken(uint256 tokenId, uint256 buyAmount) public payable {
-        require(listings[tokenId].isForSale, "This item is not for sale");
-        Listing memory listing = listings[tokenId];
+    function buyToken(uint256 listingId, uint256 buyAmount) public payable {
+        require(listings[listingId].isForSale, "This item is not for sale");
+        Listing memory listing = listings[listingId];
         uint256 totalCost = listing.price * buyAmount;
         require(msg.value >= totalCost, "Sent value is less than the total cost");
         require(tokenContract.balanceOf(listing.seller, listing.tokenId) >= buyAmount, "Seller does not have enough tokens for sale");
@@ -48,75 +59,60 @@ contract Sale {
         (bool success, ) = payable(listing.seller).call{value: totalCost}("");
         require(success, "Failed to transfer Ether to the seller");
 
-        listings[tokenId].amount -= buyAmount;
+        listings[listingId].amount -= buyAmount;
 
-        if(listings[tokenId].amount == 0) {
-            listings[tokenId].isForSale = false;
+        ownedTokens[msg.sender].push(listing.tokenId);
+
+        if(listings[listingId].amount == 0) {
+            listings[listingId].isForSale = false;
         }
     }
 
     function getAllListings() public view returns (Listing[] memory) {
-        uint256 count = 0;
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            if (listings[tokenIds[i]].isForSale) {
-                count++;
+        Listing[] memory activeListings = new Listing[](listingCount);
+        for (uint256 i = 0; i < listingCount; i++) {
+            if (listings[i].isForSale) {
+                activeListings[i] = listings[i];
             }
         }
-
-        Listing[] memory activeListings = new Listing[](count);
-        uint256 index = 0;
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            if (listings[tokenIds[i]].isForSale) {
-                activeListings[index] = listings[tokenIds[i]];
-                index++;
-            }
-        }
-
         return activeListings;
     }
 
-    function removeListing(uint256 tokenId) public {
-        require(listings[tokenId].seller == msg.sender, "Only the seller can remove the listing");
-        listings[tokenId].isForSale = false;
+    function removeListing(uint256 listingId) public {
+        require(listings[listingId].seller == msg.sender, "Only the seller can remove the listing");
+        listings[listingId].isForSale = false;
     }
 
     function getListingsByUser(address user) public view returns (Listing[] memory) {
-        uint count = 0;
-        for (uint i = 0; i < tokenIds.length; i++) {
-            if (listings[tokenIds[i]].seller == user && listings[tokenIds[i]].isForSale) {
+        uint256[] memory userListingIds = new uint256[](listingCount);
+        uint256 count = 0;
+        for (uint256 i = 0; i < listingCount; i++) {
+            if (listings[i].seller == user && listings[i].isForSale) {
+                userListingIds[count] = i;
                 count++;
             }
         }
 
         Listing[] memory userlistings = new Listing[](count);
-        uint index = 0;
-        for (uint i = 0; i < tokenIds.length; i++) {
-            if (listings[tokenIds[i]].seller == user && listings[tokenIds[i]].isForSale) {
-                userlistings[index] = listings[tokenIds[i]];
-                index++;
-            }
+        for (uint256 i = 0; i < count; i++) {
+            userlistings[i] = listings[userListingIds[i]];
         }
 
         return userlistings;
     }
 
+    function getListingsByToken(uint256 tokenId) public view returns (Listing[] memory) {
+        uint256[] memory listingIds = tokenToListings[tokenId];
+        uint256 count = listingIds.length;
+
+        Listing[] memory tokenListings = new Listing[](count);
+        for (uint256 i = 0; i < count; i++) {
+            tokenListings[i] = listings[listingIds[i]];
+        }
+        return tokenListings;
+    }
+
     function getOwnedTokens(address owner) public view returns (uint256[] memory) {
-        uint256 count = 0;
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            if (tokenContract.balanceOf(owner, tokenIds[i]) > 0) {
-                count++;
-            }
-        }
-
-        uint256[] memory ownerTokens = new uint256[](count);
-        uint256 index = 0;
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            if (tokenContract.balanceOf(owner, tokenIds[i]) > 0) {
-                ownerTokens[index] = tokenIds[i];
-                index++;
-            }
-        }
-
-        return ownerTokens;
+        return ownedTokens[owner];
     }
 }
